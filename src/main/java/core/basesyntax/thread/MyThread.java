6d17c5927393ecc75af890d30f84c03556d68ade
Tamara-Paskalova <forptv@gmail.com>
2021-08-31 13:28:32 +0300

    created 2 threads

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,10 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I'm a daemon thread! A-ha-ha!!!");
+        }
+    }
 }
