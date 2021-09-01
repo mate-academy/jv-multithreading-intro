1f4f5a768f27a8c56ecf5b22b2d42d216bf0b874
sSlonIk <kryachok@gmail.com>
2021-09-01 12:12:46 +0300

    travis fixed

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -3,8 +3,9 @@ package core.basesyntax.thread;
 public class MyThread extends Thread {
     @Override
     public void run() {
-        while(true) {
-            System.out.println("I am the daemon thread! I Came Here to Drink Milk and Kick Ass, and I've Just Finished My Milk");
+        while (true) {
+            System.out.println("I am the daemon thread!"
+                    + " I Came Here to Drink Milk and Kick Ass, and I've Just Finished My Milk");
         }
     }
 }
