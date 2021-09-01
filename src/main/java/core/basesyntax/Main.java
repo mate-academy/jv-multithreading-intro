544357d943452d26bd359314f4a1f32a17204d78
Nikitozin <nikitozin91@gmail.com>
2021-09-01 08:36:48 +0300

    Solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,21 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myTread = new Thread(() -> {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println(i);
+            }
+        });
+        Thread myDaemonThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println("I am the daemon thread!");
+                }
+            }
+        });
+        myTread.start();
+        myDaemonThread.setDaemon(true);
+        myDaemonThread.start();
     }
 }
