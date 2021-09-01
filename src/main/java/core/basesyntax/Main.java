2ff96928222d2f85fbd26041874d2a762f1b8da6
sSlonIk <kryachok@gmail.com>
2021-09-01 12:10:24 +0300

    daemon created

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread t = new MyThread();
+        t.setDaemon(true);
+        t.start();
+
+        Thread r = new Thread(new MyRunnable());
+        r.start();
     }
 }
