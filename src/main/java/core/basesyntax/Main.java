4bb02ab2d4b7a1fda9ff208c83d53a36e7aa9243
Kostia Dementiev <konstde00@gmail.com>
2021-10-08 23:50:25 +0300

    fixed style mistakes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Thread myThread = new MyThread();
         Runnable myRunnable = new MyRunnable();
-        Thread DaemonThread = new Thread(myRunnable);
+        Thread daemonThread = new Thread(myRunnable);
 
-        DaemonThread.setDaemon(true);
-        DaemonThread.start();
+        daemonThread.setDaemon(true);
+        daemonThread.start();
         myThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -2,7 +2,7 @@ package core.basesyntax.thread;
 
 import java.util.stream.IntStream;
 
-public class MyThread extends Thread{
+public class MyThread extends Thread {
     @Override
     public void run() {
         IntStream.range(0, 21).forEach(System.out::println);
