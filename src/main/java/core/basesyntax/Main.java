ee35fe545439cadb50bc1dad1d5690270e10ce1b
Vladyslav <lehter319@gmai.com>
2021-11-19 19:58:06 +0200

    Completed task.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
+    public static final boolean SET_DAEMON = true;
+
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+        Thread runnableThread = new Thread(new MyRunnable());
+        myThread.setDaemon(SET_DAEMON);
+        myThread.start();
+        runnableThread.start();
     }
 }
