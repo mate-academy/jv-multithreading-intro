894eca2e7b0d235359af5d54d5348099d32baf3f
Taras Oleksiuk <64101459+taras-oleksiuk@users.noreply.github.com>
2021-11-20 15:44:08 +0200

    Solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Runnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+        myRunnableThread.start();
+        Thread myThreadDaemon = new MyThread();
+        myThreadDaemon.setDaemon(true);
+        myThreadDaemon.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,15 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+import java.util.Random;
+
+public class MyRunnable implements Runnable {
+    private static int numberValue = 0;
+
+    @Override
+    public void run() {
+        numberValue = new Random().nextInt(20);
+        System.out.println("The MyRunnable thread is running "
+                + Thread.currentThread().getName()
+                + " the number is: " + numberValue);
+    }
 }
