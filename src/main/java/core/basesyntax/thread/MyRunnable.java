940dd9b54174443f99cfb4519f1883b13afa7ad7
Gleb Kreshchuk <gleb.net@gmail.com>
2021-11-22 23:57:37 +0200

    jv-multithreading intro task completed

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+        while (true) {
+             System.out.println(String.format("I'm %s - a daemon thread!", currentThread.getName()));
+        }
+    }
 }
