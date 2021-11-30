719230a05bc3ac333d7af16a9a8aa808d8b0d176
Serhii Orlov <orlov30@gmail.com>
2021-11-30 08:25:25 +0200

    Implements two custom threads

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am the daemon thread! Thread: "
+                    + Thread.currentThread().getName());
+        }
+    }
 }
