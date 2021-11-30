26c959a5244b7fe3a14b5e56837f023d37ca811d
V <v.ivashchenko42@protonmail.com>
2021-11-30 21:58:32 +0300

    created myThread and MyRunnable

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+        Thread myRunnableThread = new Thread(new MyRunnable());
+
+        myThread.setDaemon(true);
+        myThread.start();
+        myRunnableThread.start();
     }
 }
