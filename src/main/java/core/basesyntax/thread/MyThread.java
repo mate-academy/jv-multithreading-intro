f435470478b5e59a85640a010e215a718fc63838
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2021-12-04 13:17:15 +0200

    added solution

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+import java.util.Random;
+
+public class MyThread extends Thread {
+    private static ThreadLocal<Integer> number = new ThreadLocal<>();
+
+    @Override
+    public void run() {
+        number.set(new Random().nextInt(10));
+        System.out.println(number.get());
+    }
 }
