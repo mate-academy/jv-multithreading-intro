911c2f91aff0f0ada2eb41e358eeed7fe53dd019
Roman Polulikh <polilikh.r@gmail.com>
2022-01-17 16:43:36 +0200

    implemented all requirements

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread firstThread = new MyThread();
+        Runnable runnable = new MyRunnable();
+        Thread secondThread = new Thread(runnable);
+        secondThread.setDaemon(true);
+        firstThread.start();
+        secondThread.start();
     }
 }
