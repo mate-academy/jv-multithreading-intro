f809e0b9ebf74cda648cb2b55ff2fb0353788935
Olha <FreakyStyley85@gmail.com>
2022-01-27 10:00:28 +0200

    changed variable number

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -3,11 +3,9 @@ package core.basesyntax.thread;
 import java.util.Random;
 
 public class MyThread extends Thread {
-    private int number;
-
     @Override
     public void run() {
-        number = new Random().nextInt(20);
+        int number = new Random().nextInt(20);
         System.out.println(number);
     }
 }
