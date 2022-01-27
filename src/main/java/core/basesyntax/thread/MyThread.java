57b91ecc1645f2212c6855e42362e0a2d86a898f
Olha <FreakyStyley85@gmail.com>
2022-01-27 08:22:04 +0200

    created two threads

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+import java.util.Random;
+
+public class MyThread extends Thread {
+    private int number;
+    @Override
+    public void run() {
+        number = new Random().nextInt(20);
+        System.out.println(number);
+    }
 }
