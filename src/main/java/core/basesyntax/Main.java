442e7ffcee3a20defda4af349489f866eab93cae
darina.tolstykh <to.darina.to@gmail.com>
2022-03-16 10:11:01 +0200

    logic part of task done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Runnable thread = new MyRunnable();
+        Thread runnableThread = new Thread(thread);
+        Thread demonThread = new MyThread();
+        demonThread.setDaemon(true);
+        runnableThread.start();
+        demonThread.start();
     }
 }
