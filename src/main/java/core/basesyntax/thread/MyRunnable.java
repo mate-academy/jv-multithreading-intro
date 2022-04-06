b147281deca7767e7233407443ee9b3b16b4569e
Aleksandr Savchenko <savchenko.arts@gmail.com>
2022-04-06 22:14:16 +0300

    HW-Multithreading-intro

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    private static volatile String string = "I am the daemon thread!";
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(string);
+        }
+    }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    private static final int TWENTY = 20;
+    @Override
+    public void run() {
+        for (int i = 0; i <= TWENTY; i++) {
+            System.out.println(i);
+        }
+    }
 }
