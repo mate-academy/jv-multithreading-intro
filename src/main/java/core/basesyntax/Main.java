f4d759e9b6d231e766d114ec2cfe6f5a80a9e060
Stanislav Aleksandrovych <stanislav.aleksandrovych@refinitiv.com>
2022-04-12 00:31:28 +0300

    Multithreading introduction homework

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread runnableThread = new Thread(new MyRunnable());
+        runnableThread.setDaemon(true);
+        runnableThread.start();
+
+        Thread thread = new MyThread();
+        thread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        String string = "I am the daemon thread!";
+        while (true) {
+            System.out.println(string);
+        }
+    }
 }
