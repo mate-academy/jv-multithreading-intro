aea02544477a8227c1bd391de1d612014e1c9e38
Liubov Chernohor <liubovchernohor@gmail.com>
2022-05-08 08:54:09 +0300

    Threads were implemented

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+        myThread.setDaemon(true);
+        Runnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+        myThread.start();
+        myRunnableThread.start();
     }
 }
