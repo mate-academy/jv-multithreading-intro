40b9b37d02b006496311270ed3612cf6f9143c2d
IgorZima90 <zimyshkaa@gmail.com>
2022-05-10 03:27:23 +0300

    created multithreading

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        int start = 0;
+        while (start <= 20) {
+            System.out.println(start);
+            start++;
+        }
+    }
 }
