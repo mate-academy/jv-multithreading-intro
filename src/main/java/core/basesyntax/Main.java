49c9f7e75c4d4ea49ebf0dacf4128a7b62f26357
oleg-new <9213195@gmail.com>
2022-08-04 22:17:27 +0300

    two threads are created, one of which is a daemon

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        MyThread myThread = new MyThread();
+        Thread newThread = new Thread(myRunnable);
+        newThread.setDaemon(true);
+        newThread.start();
+        myThread.start();
     }
 }
