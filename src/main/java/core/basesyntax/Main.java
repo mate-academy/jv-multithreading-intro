d1777bed8bfe0e4a2aa922afcc3931f4d6b2ef27
Mykola Yakymiv <mykola8426@gmail.com>
2022-08-13 17:47:38 +0300

    thread and deamon thread were created

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread thread = new MyThread();
+        thread.start();
+
+        MyRunnable runnable = new MyRunnable();
+        Thread daemon = new Thread(runnable);
+        daemon.setDaemon(true);
+        daemon.start();
     }
 }
