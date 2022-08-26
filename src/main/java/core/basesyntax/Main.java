e48908153187b7c46132a05bbda5bd8ddac9fa69
agash <alexgash8@gmail.com>
2022-08-26 14:46:43 +0300

    multithreading works

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+        Thread myDeamonThread = new Thread(new MyRunnable());
+        myDeamonThread.setDaemon(true);
+        myThread.start();
+        myDeamonThread.start();
+        System.out.println("The programm is running in: " + Thread.currentThread().getName());
     }
 }
