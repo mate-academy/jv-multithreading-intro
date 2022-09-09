a8c7a475638e6906cc220838dc47733c4161c553
Denys.Sh <7860denis@gmail.com>
2022-09-09 12:41:53 +0300

    solution is done

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Logic inside MyThread thread: "
+                + Thread.currentThread().getName());
+        for (int i = 0; i < 20; i++) {
+            System.out.println("My number - " + i);
+        }
+    }
 }
