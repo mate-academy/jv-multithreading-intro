4e50044a7fb21a3796e06c80b3237ad3c106bf1f
drapail <mv.dibrov@gmail.com>
2022-09-18 23:04:41 +0300

    fucntionality implemented

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Thread classThread = new MyThread();
+        classThread.start();
+        Runnable interfaceRunnable = new MyRunnable();
+        Thread interfaceThread = new Thread(interfaceRunnable);
+        interfaceThread.setDaemon(true);
+        interfaceThread.start();
+
     }
 }
