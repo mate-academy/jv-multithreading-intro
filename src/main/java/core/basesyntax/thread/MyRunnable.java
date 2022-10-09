7ea6ee2d0c7eb56b90e94699da16cccbc5c7b01d
ssplot56 <ssplot56@gmail.com>
2022-10-09 16:35:42 +0300

    done

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -3,6 +3,8 @@ package core.basesyntax.thread;
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
-        System.out.println("I am the daemon thread!");
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
     }
 }
