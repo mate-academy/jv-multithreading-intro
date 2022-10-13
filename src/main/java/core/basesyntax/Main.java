9fd02f8555ed48857d5e0642103eccd1448381c0
Oleksii Mospan <alexey.mospan@gmail.com>
2022-10-13 13:12:07 +0300

    Initial commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread firstThread = new MyThread();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread secondThread = new Thread(myRunnable);
+        firstThread.start();
+        secondThread.setDaemon(true);
+        secondThread.start();
     }
 }
