7039daeae6349ef5ff97ae8ce6976ff23f1e0dc9
buuufy <s1mpleworkpost@gmail.com>
2022-10-30 14:43:43 +0200

    add solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread firstThread = new MyThread();
+        firstThread.start();
+        MyRunnable myRunnableThread = new MyRunnable();
+        Thread secondThread = new Thread(myRunnableThread);
+        secondThread.setDaemon(true);
+        secondThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,9 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+import java.util.Random;
+
+public class MyThread extends Thread {
+    public void run() {
+        System.out.println(new Random().nextInt(20));
+    }
 }
