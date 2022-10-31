9b3df6ce340e27860a8ab3de993c4e59f5b5dd93
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-31 17:12:42 +0100

    add solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread firstThread = new MyThread();
+        Runnable secondThreat = new MyRunnable();
+        Thread secondThread = new Thread(secondThreat);
+
+        firstThread.start();
+        secondThread.start();
+
     }
 }
