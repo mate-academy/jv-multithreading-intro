c9a3f7938a4ab43a2e45548e33f074144e69200c
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-10-31 15:46:00 +0200

    Created and started in `main()` method 2 threads: the first extending `Thread`, and the second implementing `Runnable`.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread first = new MyThread();
+        Runnable myRunnable = new MyRunnable();
+        Thread second = new Thread(myRunnable);
+        second.setDaemon(true);
+        first.start();
+        second.start();
     }
 }
