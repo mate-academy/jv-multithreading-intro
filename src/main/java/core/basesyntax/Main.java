9e1a0f1ef10a21f6ed0ca02a1644c7f3609d6530
vlad <vladsysa96@gmail.com>
2022-11-04 12:18:18 +0100

    created 1 daemon thread and 1 user thread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+
+        MyThread myThread = new MyThread();
+        myThread.setDaemon(true);
+        myThread.start();
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+        myRunnableThread.start();
     }
 }
