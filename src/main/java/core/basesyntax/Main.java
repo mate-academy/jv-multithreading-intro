9597725ed1c24bda6642c27cb4262df0f1d5da0d
Yura Hryniv <superyura444@gmail.com>
2022-12-16 18:53:49 +0200

    Multithreading practice

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Runnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+        myRunnableThread.setDaemon(true);
+        myRunnableThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+import java.util.Random;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        int random = new Random().nextInt(20);
+        for (int i = 0; i < random; i++) {
+            System.out.println("Counting numbers: " + i);
+        }
+    }
 }
