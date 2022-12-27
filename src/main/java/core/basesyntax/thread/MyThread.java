9514f65751cc851ec5a5e0ca7c75c7fcb7859dec
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-12-27 15:00:00 +0200

    created MyThread and MyRunnableThread

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        int i = 0;
+        while (i <= 20) {
+            System.out.println(i++);
+        }
+    }
 }
