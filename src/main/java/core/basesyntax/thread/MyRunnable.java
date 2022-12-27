4a56f8410e8be495a52536deb2289f5c78306240
Serhii Prykhodko <sergii.prykhodko@gmail.com>
2022-12-27 22:05:03 +0200

    mvn fix

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,12 +1,12 @@
 package core.basesyntax.thread;
 
 public class MyRunnable implements Runnable {
-    int MAX_VALUE = 20;
+    private static final int MAX_VALUE = 20;
 
     @Override
     public void run() {
         int counter = 0;
-        while (counter <= MAX_VALUE)    {
+        while (counter <= MAX_VALUE) {
             System.out.println(counter++);
         }
     }
