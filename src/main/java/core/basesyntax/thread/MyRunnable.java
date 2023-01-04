bd558ea1b9f50a19b33ac923b8a69a9d44608d6f
yeh.breus <yeh.breus@gmail.com>
2023-01-04 12:51:12 +0200

    mt intro task complete

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        int count = 0;
+        while (count < 20) {
+            System.out.println(count);
+            count++;
+        }
+    }
 }
