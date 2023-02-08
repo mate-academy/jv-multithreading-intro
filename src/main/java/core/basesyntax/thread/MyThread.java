f50bb7be065e12ddc1f210b5fad92f6e3010a772
Riki Maru <qui.ego@icloud.com>
2023-02-08 15:43:52 +0200

    created and implemented threads

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        int count = 0;
+        while (count <= 20) {
+            System.out.println(count);
+            count++;
+        }
+    }
 }
