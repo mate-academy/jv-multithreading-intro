973b0ab1a49f79b821f5f341adef4a50ad3cd590
Mykola Radzivon <kolian201497@gmail.com>
2023-03-08 21:57:46 +0200

    add loop

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -3,6 +3,8 @@ package core.basesyntax.thread;
 public class MyThread extends Thread {
     @Override
     public void run() {
-        System.out.println("I am the daemon thread!");
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
     }
 }
