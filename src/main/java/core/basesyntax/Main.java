1f84cecdd123427076728b0de3a59f366cf3e63b
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2023-03-22 21:47:35 +0200

    Threads created

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Main method was called \n\n----------------------\n");
+
+        Thread myThread = new MyThread();
+        Runnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+        myRunnableThread.setDaemon(true);
+
+        myThread.start();
+        myRunnableThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,16 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Logic inside MyRunnable thread. " + Thread.currentThread().getName());
+        while (true) {
+            System.out.println("I am daemon thread!");
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
