152eee219cd26c1727aae6563aecb68f9057239e
ArtemMakovskyy <photomak79@gmail.com>
2023-04-12 14:10:57 +0300

    task multithreading solved

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    private static final int END_NUMBER = 20;
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= END_NUMBER; i++) {
+            System.out.println(i);
+        }
+    }
 }
