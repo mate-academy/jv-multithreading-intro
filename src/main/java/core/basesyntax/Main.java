bf975d98bb5a6ea6157763703120504595bac7af
kromchk <kromchyk@yahoo.com>
2023-04-16 08:16:06 +0300

    realization of multithreading-intro

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,19 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
+    static final MyThread mythread = new MyThread();
+    static final MyRunnable myRunnable = new MyRunnable();
+    static final Thread myRunnableThread = new Thread(myRunnable);
+
     public static void main(String[] args) {
-        // write your code here
+        // Create Thread objects that extends Thread and implement Runnable interface as targets
+        // Start the threads
+        System.out.println("Launch MyThread class");
+        mythread.start();
+        System.out.println("Launch MyRunnable class");
+        myRunnableThread.start();
     }
 }
