731bb5bf8fc7283091ad4ca2a430b7afdec2e421
WallRun <dimonty2003@gmail.com>
2023-04-20 20:38:24 +0300

    Initial commit

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,15 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    public MyThread() {
+        setDaemon(true);
+
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
+    }
 }
