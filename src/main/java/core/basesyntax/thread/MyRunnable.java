c6dcc7cce3d8027f8a1e5ee4d7f8699fbb643eeb
Denis Pugovkin <den.pugovkin@gmail.com>
2023-04-28 21:33:13 +0300

    1. Created two threads classes with methods. 2. Checked all functional in main.

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+import java.util.Random;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        while (true)
+        System.out.println(new Random().nextInt(20));
+    }
 }
