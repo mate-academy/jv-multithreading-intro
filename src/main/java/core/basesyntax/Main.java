895fd401e01308a1534037e0404e947b6833039e
Sergiy Golubchenko <galuba1@gmail.com>
2023-04-28 19:40:00 +0300

    Create 2 thread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+        myRunnableThread.setDaemon(true);
+
+        MyThread myThread = new MyThread();
+
+        myThread.start();
+        myRunnableThread.start();
     }
 }
