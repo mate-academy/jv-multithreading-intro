79245cc0ed30d0207541365cc200c2d65471f12b
Denys Dominskyi <dominskyj@gmail.com>
2023-05-10 13:38:40 +0300

    Initial commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        myRunnable.run();
+        MyThread myThread = new MyThread();
+        myThread.setDaemon(true);
+        myThread.start();
     }
 }
