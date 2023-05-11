8e31bbd030404b4251300ab58cda629b86c274a1
Kyrylo Hyliuk <kyryl.huliuk@gmail.com>
2023-05-11 12:57:46 +0300

    Added hw solution

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class MyRunnable implements Runnable {
+    private final Random random = new SecureRandom();
+
+    @Override
+    public void run() {
+        System.out.println("random number: " + random.nextInt(20));
+    }
 }
