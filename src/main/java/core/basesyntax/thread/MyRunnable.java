250d1bde765d491143c90a9970418797ada57f4a
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-05-11 14:21:25 +0300

    Create two threads

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            for (int i = 0; i < 20; i++) {
+                System.out.println(i);
+            }
+        }
+    }
 }
