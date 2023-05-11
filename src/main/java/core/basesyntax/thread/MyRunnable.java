f9773cda17347ebcea333ad57d4027790a161f9e
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-05-11 18:20:18 +0300

    implemented 2 classes for threads

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
+import java.util.stream.IntStream;
+
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        IntStream.iterate(0, i -> i + 1).forEach(System.out::println);
+    }
     // write your code here
 }
