1dd4a53298ba2bee872de171a0774e2958211d36
potrap <potrapmax@gmail.com>
2023-05-11 17:43:37 +0300

    jv-multithreading-intro solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread = new MyThread();
+        Thread runnableThread = new Thread(new MyRunnable());
+        runnableThread.setDaemon(true);
+        thread.start();
+        runnableThread.start();
     }
 }
