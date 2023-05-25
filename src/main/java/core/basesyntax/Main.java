a60b0820047fa35fd19bfe3b331190dde53f42d7
Dima Senko <d.senko@i.ua>
2023-05-25 11:15:50 +0300

    commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread firstThread = new MyThread();
+        firstThread.setDaemon(true);
+        firstThread.start();
+        Thread secondThread = new Thread(new MyRunnable());
+        secondThread.start();
     }
 }
