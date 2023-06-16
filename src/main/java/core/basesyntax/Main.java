4129aee6f98fcfe0eaa6d7ee23854c4e790e349d
Arina <crowlynx4@gmail.com>
2023-06-16 17:58:27 +0300

    fixed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,11 @@
 package core.basesyntax;
 
 import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
 
 public class Main {
     public static void main(String[] args) {
-        Thread myThread = new Thread();
+        Thread myThread = new MyThread();
         Runnable myRunnable = new MyRunnable();
         Thread myRunnableThread = new Thread(myRunnable);
 
