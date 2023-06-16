ee79f5130b751c31cdeb8b302a7fa129ea33bd1d
Arina <crowlynx4@gmail.com>
2023-06-16 17:57:30 +0300

    created in my runnable interface the infinite loop; created in my thread print numbers from 0 to 20; created my thread in main method; created daemon thread my runnable in main method; mvn checked

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new Thread();
+        Runnable myRunnable = new MyRunnable();
+        Thread myRunnableThread = new Thread(myRunnable);
+
+        myThread.start();
+        myRunnableThread.setDaemon(true);
+        myRunnableThread.start();
     }
 }
