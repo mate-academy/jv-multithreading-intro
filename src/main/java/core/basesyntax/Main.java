f6079efe5a654605aff78cfe9ec77eeac21064c3
ASelivonchyk <aselivonchyk@gmail.com>
2023-06-23 11:59:05 +0300

    Solved task jv-multithreading-intro - implemented method run() in MyRunnable.class that print numbers from 0 to 20; - implemented method run() in MyThread.class that print some message to console; -in Main.class created instances of MyRunnable.class,MyThread.class, made MyThread instance as daemon thread and start the threads.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread firstThread = new MyThread();
+        firstThread.setDaemon(true);
+        firstThread.start();
+        MyRunnable secondThread = new MyRunnable();
+        Thread myRunnableTread = new Thread(secondThread);
+        myRunnableTread.start();
     }
 }
