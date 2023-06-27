e23bbd3c527fc6db0d389ca2c7f6d919775262b4
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-06-27 13:28:54 +0300

    Create and start

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        int a = 0;
+        for (int i = 0; i < 21; i++) {
+            System.out.println(a);
+            a++;
+        }
+    }
 }
