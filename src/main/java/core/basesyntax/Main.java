1ad11ed9fbaf5c22926db26f31435f1071bf45db
richn <rolik.alexx@gmail.com>
2023-06-29 04:34:28 +0300

    initial commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+        Thread myRunnableThread = new Thread(new MyRunnable());
+        myRunnableThread.setDaemon(true);
+
+        myThread.start();
+        myRunnableThread.start();
     }
 }
