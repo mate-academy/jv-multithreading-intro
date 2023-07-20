a76c47c09168d7568d1b4fa5f55c9db83223c837
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-07-20 19:56:52 +0200

    Task jv-multithreading-1 is added for autoreview

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) throws InterruptedException {
+        MyThread myThread = new MyThread();
+        Thread myRunnable = new Thread(new MyRunnable());
+        myRunnable.start();
+        myThread.setDaemon(true);
+        myThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    boolean print = true;
+
+    public void run() {
+        while (print) {
+            System.out.println("I am the daemon thread!");
+        }
+    }
 }
