561f58f40ec4e79315595d02d6cb58f7e8c9191c
Vasyl Gladkyi <vasyl.gladky@gmail.com>
2023-08-06 17:39:11 +0300

    commit

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,11 +1,10 @@
 package core.basesyntax.thread;
 
 public class MyThread extends Thread {
-   @Override
-   public void run() {
-      for (int i = 0; i <= 20; i++) {
-         System.out.println(i);
-      }
-   }
-
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
 }
