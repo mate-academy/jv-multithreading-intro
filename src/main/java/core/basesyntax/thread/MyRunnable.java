1e1d7066f0ccd50d6737ac2eec8953a22a5f133f
Gamemod13 <igorfetov1997@gmail.com>
2023-08-07 10:48:50 +0300

    Update checkstyle

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,10 +1,10 @@
 package core.basesyntax.thread;
 
-public class MyRunnable implements Runnable{
+public class MyRunnable implements Runnable {
     @Override
     public void run() {
-        while (true){
-            System.out.println( "I am the daemon thread!" );
+        while (true) {
+            System.out.println("I am the daemon thread!");
         }
     }
     // write your code here
