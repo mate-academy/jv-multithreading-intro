c39c36ca23a51bb7e40a09668c4ffe5aa1f37254
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-08-09 17:58:09 +0300

    created all requiremented threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+       Thread customThread = new MyThread();
+       Runnable myRunnable = new MyRunnable();
+       Thread customDeamonThread = new Thread(myRunnable);
+       customDeamonThread.setDaemon(true);
+
+       customDeamonThread.start();
+       customThread.start();
     }
 }
