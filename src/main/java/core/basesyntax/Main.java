4a08a969b2c54042585d6f38648c46761664c983
Volodymyr Rusyn <volodymyrruso@gmail.com>
2023-09-04 11:57:20 +0200

    solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread1 = new Thread(new MyThread());
+        thread1.start();
+
+        Thread thread2 = new Thread(new MyRunnable());
+        thread2.setDaemon(true);
+        thread2.start();
     }
 }
