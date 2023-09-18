4271bd8cc97c7df3119998fc4cd697def8c682b5
-admin- <viktoriiamaherko@gmail.com>
2023-09-18 18:55:26 +0300

    Created 2 threads

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    private int number = 0;
+
+    @Override
+    public void run() {
+        while (number <= 20) {
+            System.out.println(number);
+            number++;
+        }
+    }
 }
