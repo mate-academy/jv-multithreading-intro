cebad4dd27144c18a92d67c9410d821137a33b98
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-10-06 18:45:21 +0300

    Created and started in main() method 2 threads: the first extending Thread, and the second implementing Runnable. One of the threads prints out numbers from 0 to 20. The other one contains an infinite loop where the message "I am the daemon thread!" is printed to the console. Made this thread a daemon

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,10 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        for(int i = 0; i < 21; i++) {
+            System.out.println(i);
+        }
+    }
 }
