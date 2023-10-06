947e1ff38c5e22bc9f2a7636f4ee3fbe1f04008e
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-10-06 23:28:25 +0300

    implemented require threads runnable and daemon thread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable runnableThread = new MyRunnable();
+        Thread userThread = new Thread(runnableThread);
+        Thread daemonThread = new MyThread();
+        daemonThread.setDaemon(true);
+        userThread.start();
+        daemonThread.start();
     }
 }
