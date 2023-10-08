a91d48a6193c9f5d09d3e73ff34121282e8f075a
Ihor Kalaur <44139757+IhorKalaur@users.noreply.github.com>
2023-10-08 23:48:11 +0300

    deleted space

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -3,7 +3,7 @@ package core.basesyntax.thread;
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
-        for (int i = 0; i <=20; i++) {
+        for(int i = 0; i <=20; i++) {
             System.out.println("Number from " + Thread.currentThread().getName()
                     + " " + i);
         }
