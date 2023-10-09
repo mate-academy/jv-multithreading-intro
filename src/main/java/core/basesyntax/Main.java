7a3214d95ad1c7f92c8724dbbb7c7348520339e9
Dmytro Martyshchuk <Di.mart.ap@gmail.com>
2023-10-09 10:17:08 +0300

    Create MyRunnable and MyThread classes

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+        Thread daemon = new Thread(new MyRunnable());
+        daemon.setDaemon(true);
+        myThread.start();
+        daemon.start();
     }
 }
