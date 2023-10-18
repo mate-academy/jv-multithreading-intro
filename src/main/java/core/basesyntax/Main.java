5c4b68e6c8ea8f21da971d0aea5c24aa986c7177
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-10-18 12:40:54 +0300

    first version

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread newThread = new MyThread();
+        MyRunnable runnableThread = new MyRunnable();
+        Thread newRunableThread = new Thread(runnableThread);
+        newRunableThread.setDaemon(true);
+        newRunableThread.start();
+        newThread.start();
     }
 }
