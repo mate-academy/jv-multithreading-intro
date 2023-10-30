0939585abad0b84078c1e002af0b67847b122531
Melnychuk <melnychukmyhailo@gmail.com>
2023-10-30 11:40:15 +0200

    MyThread and MyRunnable were edited and tested in main class.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable runnable = new MyRunnable();
+        Thread runnableThread = new Thread(runnable);
+        MyThread thread = new MyThread();
+
+        runnableThread.start();
+        thread.start();
     }
 }
