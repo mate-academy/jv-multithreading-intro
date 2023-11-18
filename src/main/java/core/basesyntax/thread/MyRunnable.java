a39ce266cbc25d6c64279b1571f05e2b29c9f6e2
Watxesnavo <makasin_12@hotmail.com>
2023-11-18 23:30:39 +0100

    done

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
+public class MyRunnable implements Runnable {
     // write your code here
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
 }
