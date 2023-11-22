56bab26b1b734bf85db9486599f71f25d7ed559d
denysSmeiNewBom <den.smei.ds@gmail.com>
2023-11-22 11:41:58 +0200

    Refactor {

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -3,7 +3,7 @@ package core.basesyntax.thread;
 public class MyRunnable implements Runnable{
     @Override
     public void run() {
-        while (true){
+        while (true) {
             System.out.println("I am the daemon thread!");
         }
     }
