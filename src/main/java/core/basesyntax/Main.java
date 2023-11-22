8ad6cd0e60cb41a5fb3d39e3f28f764d6621e731
denysSmeiNewBom <den.smei.ds@gmail.com>
2023-11-22 11:39:17 +0200

    Completing task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+
+        MyRunnable runnableThread = new MyRunnable();
+        Thread myRunnableThread = new Thread(runnableThread);
+        myRunnableThread.setDaemon(true);
+
+        myThread.start();
+        myRunnableThread.start();
     }
 }
