c5c523724a697ed2ba55ddc7cc10eb3e0a15e83e
Mykola Mosii <mykolamosii@gmail.com>
2023-12-04 15:35:50 +0200

    created threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread daemon = new MyThread();
+        Thread thread = new Thread(new MyRunnable());
+        daemon.setDaemon(true);
+        daemon.start();
+        thread.start();
     }
 }
