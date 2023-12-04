3bdf7fd3e899cbd1b6444f2e323709317c7baa46
validatorr <volodymyrhryhoryk@gmail.com>
2023-12-04 14:08:36 +0200

    two types of threads created

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Runnable runnableThread = new MyRunnable();
+        Thread deamonThread = new Thread(runnableThread);
+        deamonThread.setDaemon(true);
+        deamonThread.start();
+
+        Thread userThread = new MyThread();
+        userThread.start();
     }
 }
