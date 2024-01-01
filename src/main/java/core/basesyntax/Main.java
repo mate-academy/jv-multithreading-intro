2ce963d0fe1bfca987f8957efa6e91d06eb66fee
Roman Yuzviak <3romanyuzviak@gmail.com>
2024-01-01 16:28:33 +0200

    done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        ThreadWithNumbers threadWithNumbers = new ThreadWithNumbers();
+        DaemonThead daemonThreadHelper = new DaemonThead();
+        Thread daemonThread = new Thread(daemonThreadHelper);
+        daemonThread.setDaemon(true);
+        threadWithNumbers.start();
+        daemonThread.start();
+    }
+
+    static class ThreadWithNumbers extends Thread {
+        @Override
+        public void run() {
+            IntStream.rangeClosed(0, 20)
+                    .forEach(System.out::println);
+        }
+    }
+
+    static class DaemonThead implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("I am the daemon thread!");
+            }
+        }
     }
 }
+
+
