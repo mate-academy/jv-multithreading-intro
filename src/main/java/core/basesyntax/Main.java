eca93e5c73bfa06396cf8cffeca3b4c846b54a88
khorkovenko <khorkovenko@promptlink.com>
2024-01-07 16:41:50 +0200

    fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.thread.MyThread;
 import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
 
 public class Main {
     public static void main(String[] args) {
