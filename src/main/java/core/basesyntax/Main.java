7b509d3fdd3bbc2113f4cb7116327050b8f9eaec
khorkovenko <khorkovenko@promptlink.com>
2024-01-07 16:40:02 +0200

    fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyThread;
+import core.basesyntax.thread.MyRunnable;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread thread1 = new MyThread();
+        Thread thread2 = new Thread(new MyRunnable());
+        thread2.setDaemon(true);
+
+        thread1.start();
+        thread2.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,27 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MyRunnable implements Runnable {
+    private volatile boolean running = true;
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    @Override
+    public void run() {
+        scheduler.scheduleAtFixedRate(() -> {
+            if (!running) {
+                System.err.println("Exiting gracefully.");
+                scheduler.shutdown();
+            } else {
+                System.out.println("I am the daemon thread!");
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public void stop() {
+        running = false;
+    }
 }
+
