78ef5749513a68600c03bcd2ab4135cc858a05e4
Igor Korolevich <igorkorolj@gmail.com>
2024-01-18 17:39:11 +0200

    added threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
+
+        Thread thread = new MyThread("daemon");
+        thread.setDaemon(true);
+        thread.start();
+        Thread thread1 = new Thread(new MyRunnable());
+        thread1.start();
         // write your code here
     }
 }
