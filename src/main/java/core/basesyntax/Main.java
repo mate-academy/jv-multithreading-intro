76d8dcd74d9f2027293a59a575e6771559f51ba2
khorkovenko <khorkovenko@promptlink.com>
2024-01-18 13:57:04 +0200

    fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -6,10 +6,11 @@ import core.basesyntax.thread.MyThread;
 public class Main {
     public static void main(String[] args) {
         MyThread thread1 = new MyThread();
+        thread1.start();
+
         Thread thread2 = new Thread(new MyRunnable());
         thread2.setDaemon(true);
-
-        thread1.start();
         thread2.start();
     }
 }
+
