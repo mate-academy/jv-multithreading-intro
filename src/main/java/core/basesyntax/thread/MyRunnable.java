07122b7ed79c2ad15c5ee63b2d95b4e629140baa
Noct2000 <oleg.kvasha.00@gmail.com>
2024-01-22 18:05:26 +0200

    implement multi thread app

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    private static final String MESSAGE = "I am the daemon thread!";
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(MESSAGE);
+        }
+    }
 }
