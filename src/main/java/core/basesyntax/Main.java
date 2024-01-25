58e53b062b961c5b9fbb068fb61d3c865f8b7e7b
Bohdan Markatov <bogdanmarkatov@gmail.com>
2024-01-26 00:19:54 +0200

    implemeted two methods

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread firstDaemonThread = new MyThread();
+        firstDaemonThread.setDaemon(true);
+        firstDaemonThread.start();
+
+        Thread secondDaemonThread = new Thread(new MyRunnable());
+        secondDaemonThread.setDaemon(true);
+        secondDaemonThread.start();
     }
 }
