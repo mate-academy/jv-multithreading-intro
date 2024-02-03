22130331de5786db2a30d3d4346246363d9a7f1d
mIst <bogdankharchenko77@gmail.com>
2024-02-03 16:06:39 +0200

    checkstyle

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -3,7 +3,7 @@ package core.basesyntax.thread;
 public class MyThread extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 21 ; i++) {
+        for (int i = 0; i < 21; i++) {
             System.out.println(i);
         }
     }
