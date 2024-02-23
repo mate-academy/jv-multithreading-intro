bf9a6dc3e1a9a8cbe854d4dfde7588e843633314
Roman Globa <globaroman@gmail.com>
2024-02-23 19:32:15 +0200

    Solving task

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,17 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    private static ThreadLocal<Integer> number = new ThreadLocal<>();
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am the daemon thread!");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
