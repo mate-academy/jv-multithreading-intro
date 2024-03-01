c0e5726916d957a85af290a13292333fd8d0f36d
Bohdan Sosunovych <bohdan.sosunovych@gmail.com>
2024-03-01 19:27:58 +0200

    solution2

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
-import core.basesyntax.thread.*;
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
 
 public class Main {
     public static void main(String[] args) {
