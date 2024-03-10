ae396a4458b3cf64b0b02a3ab0e16e494fae4b29
Oleh Shvets <olehpost69@gmail.com>
2024-03-10 11:46:01 +0300

    added_solution-multithread-intro

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+        myThread.start();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread deamonThread = new Thread(myRunnable);
+        deamonThread.setDaemon(true);
+        deamonThread.start();
     }
 }
