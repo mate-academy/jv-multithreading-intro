4aef5e737a4d013b847cee02260530785d731f03
Farion Oleksandr <sashafarion@gmail.com>
2024-03-15 19:18:53 +0200

    added fixes

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -5,6 +5,7 @@ public class MyThread extends Thread {
     public MyThread(String name) {
         super(name);
     }
+
     @Override
     public void run() {
         for (int i = 0; i <= 20; i++) {
