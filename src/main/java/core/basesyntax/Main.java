b891435bf566b6469d3a9b88b23644a2b912ea0f
Yevhenii Liashenko <paradoxrial@gmail.com>
2024-03-26 18:41:21 +0200

    make task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,10 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        myRunnable.run();
     }
 }
