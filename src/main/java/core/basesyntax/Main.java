4335925434936a27d6f142d452b0fa31bd6d8554
testSouth <southparaplan@gmail.com>
2024-04-09 00:52:04 +0300

    fixed style

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,11 +5,11 @@ import core.basesyntax.thread.MyThread;
 
 public class Main {
     public static void main(String[] args) {
-       Thread thread = new MyThread();
-       thread.setDaemon(true);
-       thread.start();
+        Thread thread = new MyThread();
+        thread.setDaemon(true);
+        thread.start();
 
-       Thread thread1 = new Thread(new MyRunnable());
-       thread1.start();
+        Thread thread1 = new Thread(new MyRunnable());
+        thread1.start();
     }
 }
