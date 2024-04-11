69574df1a8fde13a46f0de15d834b8b1141cece4
Ruslan Cherniavskiy <ruslan.od.ua@ukr.net>
2024-04-11 18:54:20 +0300

    fixed errors

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -2,7 +2,7 @@ package core.basesyntax.thread;
 
 public class MyThread extends Thread {
     public void run() {
-        for(int i = 0; i < 20; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println(i);
         }
     }
