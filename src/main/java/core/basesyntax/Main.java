82e143494838445657b523a6788039be56dced7f
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-04-12 18:26:15 +0300

    add 2 thread and 2 override for MyRunnable and MyThread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,11 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread1 = new Thread();
+        thread1.start();
+
+        Thread thread2 = new Thread();
+        thread2.setDaemon(true);
+        thread2.start();
     }
 }
