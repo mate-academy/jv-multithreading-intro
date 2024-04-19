a505e20951071b3c03a3aa4dadcb102ae74b1f8b
artem <artemsa223@gmail.com>
2024-04-19 21:31:37 +0100

    init

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,14 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
+public class MyRunnable implements Runnable {
+    /**
+     * Runs this operation.
+     */
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
+    }
     // write your code here
 }
