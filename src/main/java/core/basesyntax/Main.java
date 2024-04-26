6754515689b8a114663e77f3c3645565fa4108ff
marinakusniruk <Kushnirukmv@gmail.com>
2024-04-26 12:44:36 +0200

    Task completed

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread threadRunnable = new Thread(myRunnable);
+        Thread thread = new MyThread();
+        thread.setDaemon(true);
+        threadRunnable.start();
+        thread.start();
     }
 }
