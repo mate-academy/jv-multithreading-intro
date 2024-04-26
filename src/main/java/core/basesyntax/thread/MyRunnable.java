cf5902ea9e41e992c2d9213bce0b1f843d95ce11
marinakusniruk <Kushnirukmv@gmail.com>
2024-04-26 12:46:50 +0200

    Task completed2

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,11 +1,10 @@
 package core.basesyntax.thread;
 
-public class MyRunnable  implements Runnable {
+public class MyRunnable implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
     }
-
 }
