245f410ab9c13eb413045bccc173c67cd068c74d
Piotr Kaliszuk <p_kaliszuk@tlen.pl>
2024-05-07 17:48:59 +0200

    multithreading

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20 ; i++) {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " - " + "Number: " + i);
+        }
+    }
 }
