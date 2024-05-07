4ce97db952f4980367e1e874de7d7dd89fc50e4d
Kacper <kacperkukula97@gmail.com>
2024-05-07 19:20:15 +0200

    Implemented 2 threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+
+        Thread t1 = new MyThread();
+        MyRunnable daemonThread = new MyRunnable();
+        Thread t2 = new Thread(daemonThread);
+        t2.setDaemon(true);
+
+        t1.start();
+        t2.start();
     }
 }
