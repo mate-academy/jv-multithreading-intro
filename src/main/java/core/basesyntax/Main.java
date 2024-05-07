f95a20ab0c40b39bb0bec473cc64c29bc64d28cd
Dmytro Busyhin <busyhin.d@gmail.com>
2024-05-08 00:46:40 +0300

    feat: added default and daemon thread implementations

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,26 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        class CounterThread extends Thread {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 20; i++) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        Runnable runnable = () -> {
+            while (Thread.currentThread().isAlive()) {
+                System.out.println("I am the daemon thread!");
+            }
+        };
+
+        Thread daemonThread = new Thread(runnable);
+        daemonThread.setDaemon(true);
+        CounterThread counterThread = new CounterThread();
+
+        daemonThread.start();
+        counterThread.start();
     }
 }
