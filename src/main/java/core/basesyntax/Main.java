0e4bfea99109343ea12812cfeecb6b854c5c0452
Vitalii <impero44@gmail.com>
2024-05-15 12:35:54 +0300

    solved task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
+    private static final MyThread myThread = new MyThread();
+    private static final MyRunnable myRunnable = new MyRunnable();
+
     public static void main(String[] args) {
         // write your code here
+        Thread daemonThread = new Thread(myRunnable);
+        daemonThread.setDaemon(true);
+        myThread.start();
+        daemonThread.start();
     }
 }
