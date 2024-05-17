090b9e9461dab0c8395c7551be28633ed9099660
bobik <oliferchukrivne@i.ua>
2024-05-17 13:36:02 +0300

    done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+        Thread thread = new Thread(myThread);
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable);
+        thread1.start();
+        thread.start();
     }
 }
