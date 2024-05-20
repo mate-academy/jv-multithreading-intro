52398bb4afce84489ac2076934b5d038c616ae96
yulka-tsybulka <kasinets.ulia@gmail.com>
2024-05-20 14:28:22 +0300

    Create and start in main() method 2 threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread("Number Thread");
+        Thread myRunnable = new Thread(new MyRunnable(), "Daemon thread");
+        myRunnable.setDaemon(true);
+        myThread.start();
+        myRunnable.start();
     }
 }
