fdf3405aa9e521fd19b121546f7b182ffa4eb16b
Kristina Voytikh <61868920+kvoytikh@users.noreply.github.com>
2024-06-05 14:59:12 +0300

    multithreading-intro done
    Creating 2 classes with simple thread and runnable daemon thread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread numberThread = new MyThread();
+
+        Runnable runnableThread = new MyRunnable();
+        Thread daemonThread = new Thread(runnableThread);
+
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        numberThread.start();
+
     }
 }
