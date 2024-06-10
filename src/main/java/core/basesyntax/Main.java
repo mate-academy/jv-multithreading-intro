bd0e898113e9402406206eb22cbdabb068100b87
Eduard Fakhrutdinov <edikf03@gmail.com>
2024-06-10 19:29:38 +0300

    feat: 1 add threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,25 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread runnableThread = new Thread(myRunnable);
+        runnableThread.setDaemon(true);
+
+        myThread.start();
+        runnableThread.start();
+
+        try {
+            myThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("FINISH");
     }
 }
