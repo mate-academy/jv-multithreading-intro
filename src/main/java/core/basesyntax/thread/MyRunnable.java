3c731006d2bb2856d213c9a14e6f5332d5f2472b
VolandevlodD <vladradko2000@gmail.com>
2024-06-11 15:00:40 +0300

    task impleneted

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+
+    public static final int TIMES_TO_PRINT = 20;
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= TIMES_TO_PRINT; i++) {
+            System.out.println(i);
+        }
+    }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+
+    public static final String MESSAGE = "I am the daemon thread!";
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(MESSAGE);
+        }
+    }
 }
