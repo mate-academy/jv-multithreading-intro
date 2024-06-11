3d0fdbd2426f1e7b617ca97157e7c89de5d2a793
bodya4243 <105318502+bodya4243@users.noreply.github.com>
2024-06-11 21:04:25 +0300

    1) the task is done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread = new MyThread();
+        MyRunnable deamonThread = new MyRunnable();
+
+        thread.setDaemon(true);
+        thread.start();
+        deamonThread.run();
     }
 }
