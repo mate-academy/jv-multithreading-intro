e658e84cf91edd9289105b3584a604751060e58a
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-06-11 12:14:05 +0300

    Solution was added

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,30 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread = new MyThread();
+        Runnable daemonThread = new DaemonThread();
+        thread.start();
+        Thread daemonRunnableThread = new Thread(daemonThread);
+        daemonRunnableThread.setDaemon(true);
+        daemonRunnableThread.start();
+    }
+
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    static class DaemonThread implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("I am the daemon thread!");
+            }
+        }
     }
 }
+
