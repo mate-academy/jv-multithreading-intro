8d642b84c4eb7bd4b6ea108ae5347ba0276bbd5b
makaroshyna <katya23225@gmail.com>
2024-06-11 13:56:31 +0300

    Implement threads.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+
+        Runnable myRunnable = new MyRunnable();
+        Thread runnableThread = new Thread(myRunnable);
+        runnableThread.setDaemon(true);
+
+        myThread.start();
+        runnableThread.start();
     }
 }
