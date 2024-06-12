e581b9a99f74f285a5ec3d8de3d86de4ce18d2cb
Denys <Eidenn005@gmail.com>
2024-06-12 09:15:48 +0300

    jv multithreading solution

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread daemonThread = new Thread(myRunnable);
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+
+        MyThread threadWithNumbers = new MyThread();
+        threadWithNumbers.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,15 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am the daemon thread!");
+            try {
+                Thread.sleep(500L); // Sleep to make the output more readable
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
 }
