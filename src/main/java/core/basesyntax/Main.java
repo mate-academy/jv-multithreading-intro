d5951665c3c53b1c674349df0a9cb83e8eb9332e
Tymofii Skrypko <skrypko270901@gmail.com>
2024-06-27 20:40:05 +0200

    completed java multithreading intro

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,13 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+        MyRunnable myRunnable = new MyRunnable();
+        myThread.start();
+        myRunnable.run();
     }
 }
