8df26ec5a5c63bfb0b319a4d14482cecfa187360
Banderan <bartoszwojcik2004@gmail.com>
2024-08-08 17:17:54 +0200

    finish thread task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Runnable toThread = new MyRunnable();
+        Thread fromRunnable = new Thread(toThread);
+        fromRunnable.start();
+        Thread fromThread = new MyThread();
+        fromThread.setDaemon(true);
+        fromThread.start();
     }
 }
+
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,15 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    private static final int COUNTER = 20;
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= COUNTER; i++) {
+            System.out.println(
+                    Thread.currentThread().getName() + " : " + i
+            );
+        }
+
+    }
 }
