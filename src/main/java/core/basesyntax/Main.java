16669e66e923960605c0bb1d14f3f88f8212a188
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-08-10 01:59:21 +0300

    solve the task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+
+        MyThread firstThread = new MyThread();
+        Thread secondThread = new Thread(myRunnable);
+
+        firstThread.start();
+        secondThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,16 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
