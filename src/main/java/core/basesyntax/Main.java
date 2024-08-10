b1c7d8be0ead991daae138ac96b5454c00a48510
Oleh <mihailyakyb45@gmail.com>
2024-08-10 17:39:22 +0300

    done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyThread myThread = new MyThread();
+        MyRunnable runnable = new MyRunnable();
+        myThread.setDaemon(true);
+        myThread.start();
+        runnable.run();
     }
 }
