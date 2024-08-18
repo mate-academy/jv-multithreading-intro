7ade548559fd449da68e830dedb84ad42d91f14c
Serhii Nazarenko <nasarenko82@gmail.com>
2024-08-18 14:12:30 -0600

    the first commit in the project

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread = new MyThread();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread1 = new Thread(myRunnable);
+        thread1.setDaemon(true);
+        thread.start();
+        thread1.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,13 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    int count = 0;
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 21; i++) {
+            System.out.println(i);
+        }
+
+    }
 }
