a974ff91c749ed7781a4edddcea93d73a8d6dee0
Serhii Nazarenko <nasarenko82@gmail.com>
2024-08-19 00:26:13 -0600

    the first commit in the project

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,13 +1,10 @@
 package core.basesyntax.thread;
 
 public class MyThread extends Thread {
-    int count = 0;
-
     @Override
     public void run() {
-        for(int i = 0; i < 21; i++) {
+        for (int i = 0; i < 21; i++) {
             System.out.println(i);
         }
-
     }
 }
