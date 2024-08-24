dfb7d389dacd03692a282e47a59577c1c84d4ab7
Slava_Moiseev <qualityengineerinkom@gmail.com>
2024-08-24 19:14:12 +0300

    finished

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread first = new MyThread();
+        Thread second = new Thread(new DaemonThread());
+        second.setDaemon(true);
+        first.start();
+        second.start();
+    }
+
+    static class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0;i < 21; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    static class DaemonThread implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                System.out.println("I am the daemon thread!");
+            }
+        }
     }
 }
