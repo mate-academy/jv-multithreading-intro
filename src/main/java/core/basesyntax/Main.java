dd657a088ada383a6689956e4210b3a4da09f8e4
Artem Klishch <artemklishch@gmail.com>
2024-10-06 16:41:24 +0300

    feat: should make threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) throws InterruptedException {
+        Thread myThread = new MyThread("my thread 1");
+        myThread.start();
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread runnableThread = new Thread(myRunnable, "my runnable 1");
+        runnableThread.setDaemon(true);
+        runnableThread.start();
+
+        Thread.sleep(1000L);
     }
 }
