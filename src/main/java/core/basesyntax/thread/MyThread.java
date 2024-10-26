980be4bfaa9f5196d1548f1262742588e31eefbf
Anton <panyutin15@gmail.com>
2024-10-26 23:28:51 +0300

    Fix style problems

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,6 +1,6 @@
 package core.basesyntax.thread;
 
-public class MyThread extends Thread{
+public class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 1; i < 21; i++) {
