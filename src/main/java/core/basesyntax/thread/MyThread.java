c5d03ab693c3c6ba5efac10b97d6f68a45b15667
citruscaviar <citruscaviar96@gmail.com>
2024-11-22 17:32:37 +0300

    implemented daemon thread

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,16 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("Number: " + i);
+        }
+        try {
+            Thread.sleep(500L);
+        } catch (InterruptedException e) {
+            System.out.println("MyThread has been interrupted");
+        }
+    }
 }
