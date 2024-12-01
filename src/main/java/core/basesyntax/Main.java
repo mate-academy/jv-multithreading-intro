b3584fefb06c5cc9e637329252914b63dd00ef81
Vitaliy <vitaliyfilimonov3@gmail.com>
2024-12-01 11:01:52 +0200

    demon thread practice

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread demonThread = new MyThread();
+        Thread thread = new Thread(new MyRunnable());
+        demonThread.setDaemon(true);
+
+        demonThread.start();
+        thread.start();
     }
 }
