d059b8966002efd7fba4ecb48b8808fab4f6a535
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-12-04 21:11:17 +0200

    checkstyle fix

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,12 +1,20 @@
 package core.basesyntax.thread;
 
 public class MyThread extends Thread {
-    int count = 0;
+    private int count = 0;
 
     @Override
     public void run() {
-        while (count < 20) {
+        while (count <= 20) {
             System.out.println(count++);
         }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 }
