e182788d9edf67a649c7b0d9d340e26d105b67ef
Oleksii Deineka <deineka219@gmail.com>
2024-12-10 21:37:57 +0200

    the task was done according to the requirements

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -5,7 +5,6 @@ public class MyRunnable implements Runnable {
     public void run() {
         while (true) {
             System.out.println("I am the daemon thread!");
-
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
