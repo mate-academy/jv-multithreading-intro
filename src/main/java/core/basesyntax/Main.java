c0a474984bafffc83caf78c0c394b1b29ce6e6ad
iurii <kenu21@gmail.com>
2024-12-16 16:26:55 +0100

    THREADS: added two threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Thread thread = new Thread(new MyRunnable());
+        Thread myThread = new MyThread();
+        thread.setDaemon(true);
+        thread.start();
+        myThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
+public class MyRunnable implements Runnable {
     // write your code here
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
+    }
 }
