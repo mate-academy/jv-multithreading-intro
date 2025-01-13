0858e3a1780ee5f08c19f5727ea8c8a4cc34c491
LeonIllya <dd0733567@gmail.com>
2025-01-13 19:58:46 +0200

    My decision

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread runnableThread = new Thread(myRunnable);
+        runnableThread.start();
+
+        Thread thread = new MyThread();
+        thread.start();
     }
 }
