b5dab2bb8ac32f026089707287467651dc9f19cd
AndrewLinskii <tolstuy1996@gmail.com>
2025-01-17 15:36:26 +0200

    done

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread1 = new MyThread();
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread2 = new Thread(myRunnable, "lol");
+        thread2.setDaemon(true);
+        thread1.start();
+        thread2.start();
     }
 }
