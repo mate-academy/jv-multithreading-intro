6ebec8dfe72d62ca3128379f766d9b0e714ddc7c
AndrewLinskii <tolstuy1996@gmail.com>
2025-01-17 15:42:43 +0200

    done

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -3,9 +3,9 @@ package core.basesyntax.thread;
 public class MyRunnable implements Runnable {
     @Override
     public void run() {
-       while (true) {
-           System.out.println("I am the daemon thread!");
-       }
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
     }
-    // write your code here
+
 }
