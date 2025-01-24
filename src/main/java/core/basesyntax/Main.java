40660dbdc7e5fea627e7ecbc5915a7075fc243c4
Chorniypistolet <volkivskiy95@gmail.com>
2025-01-24 20:11:09 +0200

    jv-multithreading-intro

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+        myThread.start();
+
+        Thread myRunnable = new Thread(new MyRunnable());
+        myRunnable.setDaemon(true);
+        myRunnable.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println("Main thread interrupted");
+        }
     }
 }
