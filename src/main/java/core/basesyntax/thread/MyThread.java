34feb672324996b2dd3c6f84612ee672f45a71e1
Chorniypistolet <volkivskiy95@gmail.com>
2025-01-24 20:15:13 +0200

    jv-multithreading-intro

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -4,7 +4,7 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i <= 20; i ++) {
+        for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
     }
