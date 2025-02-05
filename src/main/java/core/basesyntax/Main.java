2b4a4bc38d8cc31661aa618b024bed704822d1fb
layzimayner <vasyahunta@gmail.com>
2025-02-05 19:56:21 +0200

    add myTread and myRunnable

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread = new MyThread();
+
+        MyRunnable runnable = new MyRunnable();
+        Thread runnableTread = new Thread(runnable);
+        runnableTread.setDaemon(true);
+
+        thread.start();
+        runnableTread.start();
     }
 }
+
