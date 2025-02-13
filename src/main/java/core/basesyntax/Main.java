0a0f4cfb0ea9437ac4bfd1955431a85fa0d8792f
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2025-02-13 17:41:16 +0000

    Implemented multithreading by creating a `Thread` subclass and a daemon `Runnable`

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,24 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread myThread = new MyThread();
+
+        Thread daemonThread = new Thread(new MyRunnable());
+        daemonThread.setDaemon(true);
+
+        myThread.start();
+        daemonThread.start();
+
+        try {
+            myThread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("Main done");
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,15 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
 }
