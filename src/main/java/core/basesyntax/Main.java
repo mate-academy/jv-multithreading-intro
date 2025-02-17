e915722e84b8c93c636bc4f72ff8a1bb65080cfa
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-02-17 13:31:52 +0100

    implemented task logic

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        firstThread();
+        secondThread();
+    }
+
+    private static void firstThread() {
+        Thread firstThread = new Thread(new MyRunnable());
+        firstThread.setDaemon(true);
+        firstThread.start();
+    }
+
+    private static void secondThread() {
+        Thread secondThread = new MyThread();
+        secondThread.start();
     }
 }
