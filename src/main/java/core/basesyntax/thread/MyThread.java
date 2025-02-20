ed898e340375023748a9e2ae7f4de688c03a8a3e
Igor Neminuschii <igorgorslav@gmail.com>
2025-02-20 15:16:27 +0200

    created two threads

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,10 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    public void run() {
+        while (true) {
+            System.out.println(Thread.currentThread().getName()
+                    + " .....  I am the daemon thread!");
+        }
+    }
 }
