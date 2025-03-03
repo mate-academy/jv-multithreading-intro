622ca9f9d1cf4013226536c1302978ee6f1fb997
Sasha <sasha.panchyshun@gmail.com>
2025-03-03 11:42:55 +0200

    jv-multithreading-intro

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Runnable runnable = new MyRunnable();
+        Thread runnableThread = new Thread(runnable);
+        runnableThread.setDaemon(true);
+        runnableThread.start();
+
+        Thread thread = new MyThread();
+        thread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,12 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("MyRunnable is created with name: "
+                + Thread.currentThread().getName());
+        while (true) {
+            System.out.println("I am the daemon thread!");
+        }
+    }
 }
