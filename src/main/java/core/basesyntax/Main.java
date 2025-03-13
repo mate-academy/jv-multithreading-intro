d461e4cf858072659a018d07876a2221886a21b4
Oleksandr Darchyk <sashadarchi@outlook.com>
2025-03-13 20:21:27 +0100

    first commit

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) throws InterruptedException {
+        MyThread t1 = new MyThread("MyThread");
+        t1.start();
+
+        MyRunnable t2 = new MyRunnable();
+        Thread runnamleThread = new Thread(t2,"MyRunnable");
+        runnamleThread.setDaemon(true);
+        runnamleThread.start();
+        Thread.sleep(1000);
     }
 }
