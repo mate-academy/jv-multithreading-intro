c0cf1019b1a3d0394e690ad8f9dcc6c736a0f37a
Mateusz Seler <mateusz.seler@gmail.com>
2025-03-14 14:16:03 +0100

    implementation

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,23 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    private int end;
+
+    public MyRunnable(int end) {
+        this.end = end;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < end; i++) {
+            System.out.println(i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
 }
