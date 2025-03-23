08033f7ba48b6396b20c4aaad32a003319e55c0d
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2025-03-23 20:26:00 +0200

    how fix 1

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -2,8 +2,8 @@ package core.basesyntax.thread;
 
 public class MyThread extends Thread {
     @Override
-    public void run(){
-        while(true){
+    public void run() {
+        while (true) {
             System.out.println("I am the daemon thread!");
         }
     }
