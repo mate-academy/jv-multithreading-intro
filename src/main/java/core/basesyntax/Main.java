88d96743aca38d16e0c222bc11e2f25dd477a439
VArabchuk <arabon3@gmail.com>
2025-03-28 11:10:14 +0200

    Multithreading intro

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable runnable = new MyRunnable();
+        Thread threadRunnable = new Thread(runnable);
+        threadRunnable.start();
+        MyThread thread = new MyThread();
+        thread.setDaemon(true);
+        thread.start();
     }
 }
