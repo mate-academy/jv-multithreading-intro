796be1b56f709033f63b17bbc602199bddf4e1ad
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2025-04-01 16:27:20 +0300

    add daemon and normal thread

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,44 @@
 package core.basesyntax;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Runnable runnable = () -> {
+                while(true) {
+                    System.out.println("I am the daemon thread!");
+                    try {
+                        sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        };
+        Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.start();
+
+        Thread thread1 = new MyThread("Prints number");
+        thread1.start();
+    }
+
+}
+
+class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("Thread " + Thread.currentThread().getName()
+                    + " prints: " + i);
+            try {
+                sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
