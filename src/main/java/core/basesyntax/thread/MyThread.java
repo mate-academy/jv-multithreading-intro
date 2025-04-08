eed38dc820b3cb119b57edb440c5c6b9db1d6ca7
Ihor Romaniuk <ihor.rome@gmail.com>
2025-04-08 18:13:19 +0300

    add solution

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,14 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    private static final Integer LIMIT = 20;
+    private static final ThreadLocal<Integer> number = new ThreadLocal<>();
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= LIMIT; i++) {
+            number.set(i);
+            System.out.println(number.get());
+        }
+    }
 }
