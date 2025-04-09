c05791638f29fb68dbe6066f89efeeadd64614b8
Panchenko Dmytro <panchenko0795@gmail.com>
2025-04-10 00:07:38 +0300

    Created 2 threads in main(): one extends Thread and prints numbers 0-20, the other implements Runnable and prints 'I am the daemon thread!' in an infinite loop as a daemon.

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread thread1 = new MyThread();
+        Runnable runnable = new MyRunnable();
+        Thread thread2 = new Thread(runnable);
+        thread2.setDaemon(true);
+        thread1.start();
+        thread2.start();
     }
 }
