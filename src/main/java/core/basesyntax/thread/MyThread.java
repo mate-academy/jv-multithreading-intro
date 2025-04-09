542414b1a8a76a33293a806b13477a952b540674
Wojtas1996PL <mat.wojtkowiak@gmail.com>
2025-04-09 18:27:19 +0200

    First solution

--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,11 @@
 package core.basesyntax.thread;
 
-public class MyThread {
-    // write your code here
+public class MyThread extends Thread {
+    public void run() {
+        int number = 0;
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(number);
+            number++;
+        }
+    }
 }
