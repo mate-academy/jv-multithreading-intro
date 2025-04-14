9d574000b8c6b0030e080c0d9c192fe63c5b9a36
Orest Sorokowey <osorokowey@gmail.com>
2025-04-14 16:10:21 +0300

    good

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) throws InterruptedException {
+        MyRunnable runnable = new MyRunnable();
+        Thread t1 = new Thread(runnable);
+        t1.start();
+
+
+        MyThread t2 = new MyThread(() -> {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println(i);
+            }
+        });
+        t2.start();
     }
 }
