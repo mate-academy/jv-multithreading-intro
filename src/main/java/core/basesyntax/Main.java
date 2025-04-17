3bbe6cd9dbdeaf042b18b439b858c68766e56e93
kupnovytskyi <kupnovytskyinazar@gmail.com>
2025-04-17 23:15:38 +0300

    created simple and daemon threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Thread thread = new MyThread();
+        thread.setDaemon(true); // We make daemon thread here.
+        thread.start();
+
+        MyRunnable myRunnable = new MyRunnable();
+        Thread anotherThread = new Thread(myRunnable);
+        anotherThread.start();
     }
 }
