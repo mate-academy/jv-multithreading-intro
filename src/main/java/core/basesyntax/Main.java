782b6804666656c3bc68ca49f202b7b09b0733a0
FroGitHub <andrey8102310@gmail.com>
2025-04-19 15:24:11 +0300

    implement MyThread and MyRunnable

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Thread daemonThread = new MyThread();
+        daemonThread.setDaemon(true);
+
+        Thread counterThread = new Thread(new MyRunnable());
+
+        daemonThread.start();
+        counterThread.start();
+
     }
 }
