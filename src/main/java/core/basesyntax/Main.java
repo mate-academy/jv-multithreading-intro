0f5533d83ad3f8d71279bd041325853a14465160
Dima Davydov <Kilo6559@gmail.com>
2025-05-09 16:18:16 +0300

    Success

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable runnable = new MyRunnable();
+        Thread t1 = new Thread(runnable);
+        t1.start();
+
+        Thread myThread = new MyThread();
+        myThread.setDaemon(true);
+        myThread.start();
     }
 }
--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -1,5 +1,14 @@
 package core.basesyntax.thread;
 
-public class MyRunnable {
-    // write your code here
+public class MyRunnable implements Runnable {
+    public void run() {
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
