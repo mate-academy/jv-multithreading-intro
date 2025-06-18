d9966a03a542c2f6233b4b51a242cd503af78a6d
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-06-18 11:56:19 +0300

    Implemented threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
         @Override
         public void run() {
-            for (int i = 0; i <= 20; i++) {
+            for (int i = 0; i <= 10; i++) {
                 i--;
                 System.out.println("I am the daemon thread!");
             }
