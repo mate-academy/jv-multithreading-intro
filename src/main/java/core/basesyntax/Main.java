952a5287f1a3ce64fd0cc10f2ed5d0279b88f36b
Serhii Holenkov <holenkov.s.s@gmail.com>
2025-06-18 11:55:21 +0300

    Implemented threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,29 @@ package core.basesyntax;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Thread t1 = new Thread(new MyRunnable());
+        t1.setDaemon(true);
+        t1.start();
+        Thread t2 = new MyThread1();
+        t2.start();
+    }
+    public static class MyThread1 extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i <= 20; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+    public static class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i <= 20; i++) {
+                i--;
+                System.out.println("I am the daemon thread!");
+            }
+        }
     }
 }
+
