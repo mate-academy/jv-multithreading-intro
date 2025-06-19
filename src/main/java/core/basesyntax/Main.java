0603eb7c89e50498d4bbbb930df097266d377640
vdsfww <loppprt@gmail.com>
2025-06-19 22:03:55 +0300

    fix

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,41 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        Thread numberThread = new NumberThread();
+        Thread numberThread = new MyThread();
 
-        Thread daemonRunnable = new Thread(new DaemonRunnable());
+        Thread daemonRunnable = new Thread(new MyRunnable());
         daemonRunnable.setDaemon(true);
 
         numberThread.start();
         daemonRunnable.start();
     }
 }
-class NumberThread extends Thread {
-    @Override
-    public void run() {
-        for (int i = 0; i <= 20; i++) {
-            System.out.println(i);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-    }
-}
 
-class DaemonRunnable implements Runnable {
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println("I am the daemon thread!");
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                break;
-            }
-        }
-    }
-}
