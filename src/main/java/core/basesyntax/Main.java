83d5b50fe183bd79fb958d9762f544a55e219e62
Dmytro Makhanok <dmmhnk23@gmail.com>
2025-07-03 20:37:09 +0200

    finished task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        MyRunnable daemonRunnable = new MyRunnable();
+        Thread daemonThread = new Thread(daemonRunnable);
+        daemonThread.setDaemon(true);
+
+        daemonThread.start();
+        myThread.start();
         // write your code here
     }
 }
--- src/main/java/core/basesyntax/thread/MyThread.java
+++ src/main/java/core/basesyntax/thread/MyThread.java
@@ -1,5 +1,16 @@
 package core.basesyntax.thread;
 
-public class MyThread {
+public class MyThread extends Thread {
+    public void run() {
+
+        for (int i = 0; i <= 20; i++) {
+            System.out.println("Number: " + i);
+            try {
+                Thread.sleep(200L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     // write your code here
 }
