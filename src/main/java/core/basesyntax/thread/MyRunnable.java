b2fee287db5d9ce3c31f309a25ba315d3da55f2d
Anton <StarAntonU@Gmail.com>
2025-07-13 14:33:04 +0300

    fixed checkstyle

--- src/main/java/core/basesyntax/thread/MyRunnable.java
+++ src/main/java/core/basesyntax/thread/MyRunnable.java
@@ -4,7 +4,7 @@ public class MyRunnable implements Runnable {
 
     @Override
     public void run() {
-        for(int i = 0; i <= 20; i++) {
+        for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
     }
