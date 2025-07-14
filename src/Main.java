5a5a412e7f993cc4afa1b460545ea85f208ee114
mykola <nikolya.cr@gmail.com>
2025-07-14 11:19:24 +0200

    final result

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 class Main {
     public static void main(String[] args) {
         Thread thread1 = new MyThread();
-
         MyRunnable runnable1 = new MyRunnable();
         Thread thread2 = new Thread(runnable1);
         thread2.setDaemon(true);
