551daad902c8140a44b71976880dc019e9e5f0d2
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-07-30 13:04:54 +0300

    solve task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import core.basesyntax.thread.MyRunnable;
+import core.basesyntax.thread.MyThread;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        MyRunnable myRunnable = new MyRunnable();
+        Thread myRunnableTread = new Thread(myRunnable, "my-runnable");
+
+        Thread myThread = new MyThread("my-thread");
+        myThread.setDaemon(true);
+
+        myThread.start();
+        myRunnableTread.start();
     }
 }
